8778954b97384c99eb3e1546e13a2e20767b909f
Walter Sylvester <walter_sylvester@msn.com>
2019-11-20 17:39:13 -0700

    set up flask show, update, and delete routes

--- resources/issues.py
+++ resources/issues.py
@@ -7,7 +7,7 @@ from playhouse.shortcuts import model_to_dict
 issue = Blueprint('issues', 'issue')
 
 
-# Get (index route)
+# Index Route (get)
 @issue.route('/', methods=["GET"])
 def get_all_issues():
     ## find the issues and change each one to a dictionary into a new array
@@ -19,7 +19,7 @@ def get_all_issues():
         return jsonify(data={}, status={"code": 401, "message": "Error getting the resources"})
 
 
-# Post Route (create)
+# Create/New Route (post)
 @issue.route('/', methods=["POST"])
 def create_issues():
     ## see request payload anagolous to req.body in express
@@ -34,3 +34,40 @@ def create_issues():
     print(model_to_dict(issue), 'model to dict')
     issue_dict = model_to_dict(issue)
     return jsonify(data=issue_dict, status={"code": 201, "message": "Success"})
+
+
+# Show Route (get)
+@issue.route('/<id>', methods=["GET"])
+def get_one_issue(id):
+    print(id, 'reserved word?')
+    issue = models.Issue.get_by_id(id)
+    print(issue.__dict__)
+    return jsonify(data=model_to_dict(issue), status={"code": 200, "message": "Success"})
+
+
+# Update/Edit Route (put)
+@issue.route('/<id>', methods=["PUT"])
+def update_issue(id):
+    payload = request.get_json()
+    # print(payload)
+
+    query = models.Issue.update(**payload).where(models.Issue.id == id)
+    query.execute()
+
+    # print(type(query))
+    # find the issue again
+    issue = models.Issue.get_by_id(id)
+
+    issue_dict = model_to_dict(issue)
+    # updated_issue = model_to_dict(query)
+    # print(updated_issue, type(update_issue))
+    return jsonify(data=issue_dict, status={"code": 200, "message": "resource updated successfully"})
+
+
+# Delete Route (delete)
+@issue.route('/<id>', methods=["DELETE"])
+def delete_issue(id):
+    query = models.Issue.delete().where(models.Issue.id==id)
+    query.execute()
+    return jsonify(data='resource successfully deleted', status="code": 200, "message": "resource deleted successfully" )
+
